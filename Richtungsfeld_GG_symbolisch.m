56faf53aebf18781c12f74a2e25c69b606bda881
Ben-L707 <56692254+Ben-L707@users.noreply.github.com>
2019-11-26 11:06:42 +0100

    Update Richtungsfeld_GG_symbolisch.m

--- Richtungsfeld_GG_symbolisch.m
+++ Richtungsfeld_GG_symbolisch.m
@@ -1,5 +1,3 @@
-
-
 o = 0.5;
 r1 = 0.3;
 r2 = 0.4;
@@ -14,14 +12,14 @@ R(x,y) = -sin(x)+y*(-r2);
 S1 = solve([P(x,y)==0, Q(x,y)==0], [x y], 'ReturnConditions', true)
 S2 = solve([P(x,y)==0, R(x,y)==0], [x,y], 'ReturnConditions', true);
 
-%gibt x-, y-Koordinaten von bis zu fünf Gleichgewichtspunkten
+%gibt x-, y-Koordinaten von bis zu fÃ¼nf Gleichgewichtspunkten
 solx1 = subs(S1.x, S1.parameters, [-2 -1 0 1 2]);
 soly1 = subs(S1.y, S1.parameters, [-2 -1 0 1 2]);
 
 solx2 = subs(S2.x, S2.parameters, [-2 -1 0 1 2]);
 soly2 = subs(S2.y, S2.parameters, [-2 -1 0 1 2]);
 
-%für Richtungsfeld
+%fÃ¼r Richtungsfeld
 [X,Y] = meshgrid(-10:1:10,-10:1:10);
 dX = P(X,Y);
 dY1 = Q(X,Y);
@@ -37,18 +35,19 @@ dX2n = dX./sqrt(dX.^2+dY2.^2);
 
 figure;
 %Richtungsfeld
-subplot(1,2,1),quiver(X,Y,dX1n,dY1n)
+subplot(1,2,1),q=quiver(X,Y,dX1n,dY1n)
+q.Color = '#DEDEDE';
+q.ShowArrowHead = 'off';
 axis tight
 hold on
 %GG-Punkte
 plot(solx1, soly1, 'o');
-subplot(1,2,2),quiver(X,Y,dX2n,dY2n)
+subplot(1,2,2),q2=quiver(X,Y,dX2n,dY2n)
+q2.Color = '#DEDEDE';
+q2.ShowArrowHead = 'off';
 axis tight
 hold on
 plot(solx2, soly2, 'o');
-
-
-
 
 
 
