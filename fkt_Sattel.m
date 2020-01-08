16464bd499e9f277b38490b1fa7ba41e7409abfb
dustin.muehl <44489469+dustinmuehl@users.noreply.github.com>
2020-01-08 13:59:44 +0100

    thisisshit

--- fkt_Sattel.m
+++ fkt_Sattel.m
@@ -1,9 +1,4 @@
-function [A,B1,B2,R] = fkt_Sattel(VJ,WJ,xValue,yValue,k,r)
-    if WJ(1,1)>WJ(2,2)
-        R = -1;
-    else
-        R = 1;
-    end
+function [A,B1,B2,R] = fkt_sattel(VJ,WJ,xValue,yValue,r,k)
     a1 = [xValue+k*VJ(1,1)+r yValue+k*VJ(2,1)+r];
     a2 = [xValue+k*VJ(1,1)+r yValue+k*VJ(2,1)-r];
     a3 = [xValue+k*VJ(1,1)-r yValue+k*VJ(2,1)+r];
@@ -13,8 +8,11 @@ function [A,B1,B2,R] = fkt_Sattel(VJ,WJ,xValue,yValue,k,r)
     a7 = [xValue-k*VJ(1,1)-r yValue-k*VJ(2,1)+r];
     a8 = [xValue-k*VJ(1,1)-r yValue-k*VJ(2,1)-r];
     A = [a1; a2; a3; a4; a5; a6; a7; a8];
-    B1 = [xValue-3*VJ(1,1) xValue+3*VJ(1,1); 
-          yValue-3*VJ(2,1) yValue+3*VJ(2,1)]; 
-    B2 =  [xValue-3*VJ(1,2) xValue+3*VJ(1,2); 
-          yValue-3*VJ(2,2) yValue+3*VJ(2,2)]; 
+    B1 = VJ(:,1); 
+    B2 = VJ(:,2);
+if WJ(1,1) < WJ(2,2)
+    R=1;
+else
+    R=-1;
+end   
 end
